da7a5011705307c7629d194b67283af69403625c
agusgeaaugusto <61166590+agusgeaaugusto@users.noreply.github.com>
2025-09-13 15:31:07 -0300

    Add files via upload

---
+++ plan.js
@@ -0,0 +1,78 @@
+
+// Planes por Enfoque (1–5)
+// Estructura: { [enfoque]: { [día]: [ {name,time,video,tips} ] } }
+window.PLANES = {
+  "1": {
+    "Lunes": [
+      {name:"Press banca + fondos", time:"40 min", video:"https://www.youtube.com/watch?v=rT7DgCr-3pg", tips:"Pecho + tríceps: 4x8–12"},
+      {name:"Aperturas mancuernas", time:"12 min", video:"https://www.youtube.com/watch?v=eozdVDA78K0"}
+    ],
+    "Martes": [
+      {name:"Bici + extensora", time:"20 + 12 min", video:"https://www.youtube.com/watch?v=1E4hXz3EWDw"},
+      {name:"Sentadilla guiada", time:"4x10", video:"https://www.youtube.com/watch?v=1oed-UmAxFs"}
+    ],
+    "Miércoles": [
+      {name:"Saltos en caja", time:"10 min", video:"https://www.youtube.com/watch?v=2q8MM9Z1G9M"},
+      {name:"Correr (intervalos)", time:"15 min", video:"https://www.youtube.com/watch?v=tJrcuKZ6STc"}
+    ],
+    "Jueves": [
+      {name:"Jalón al pecho", time:"4x10–12", video:"https://www.youtube.com/watch?v=CAwf7n6Luuc"},
+      {name:"Curl bíceps barra", time:"4x10", video:"https://www.youtube.com/watch?v=ykJmrZ5v0Oo"}
+    ],
+    "Viernes": [
+      {name:"Peso muerto rumano", time:"4x8–10", video:"https://www.youtube.com/watch?v=DJpT-KT3m3w"},
+      {name:"Curl femoral", time:"4x12", video:"https://www.youtube.com/watch?v=1Tq3QdYUuHs"}
+    ]
+  },
+  "2": {
+    "Lunes": [
+      {name:"Press inclinado + tríceps cuerda", time:"4x10", video:"https://www.youtube.com/watch?v=SrqOu55lrYU"},
+      {name:"Fondos en paralelas", time:"3x al fallo", video:"https://www.youtube.com/watch?v=2z8JmcrW-As"}
+    ],
+    "Martes": [
+      {name:"Bici HIIT", time:"15 min", video:"https://www.youtube.com/watch?v=ml6cT4AZdqI"},
+      {name:"Prensa + extensora", time:"4x12", video:"https://www.youtube.com/watch?v=IZxyjW7MPJQ"}
+    ],
+    "Miércoles": [
+      {name:"Cuerda + skipping", time:"20 min", video:"https://www.youtube.com/watch?v=1BZM0v2je6o"}
+    ],
+    "Jueves": [
+      {name:"Remo con barra", time:"4x10", video:"https://www.youtube.com/watch?v=vT2GjY_Umpw"},
+      {name:"Curl alterno", time:"4x12", video:"https://www.youtube.com/watch?v=ykJmrZ5v0Oo"}
+    ],
+    "Viernes": [
+      {name:"Puente glúteo + femoral", time:"4x12", video:"https://www.youtube.com/watch?v=9cQJptj1YxQ"}
+    ]
+  },
+  "3": {
+    "Lunes":[
+      {name:"Press mancuernas + tríceps banco", time:"4x10", video:"https://www.youtube.com/watch?v=VmB1G1K7v94"}
+    ],
+    "Martes":[
+      {name:"Extensora + sentadilla goblet", time:"4x12", video:"https://www.youtube.com/watch?v=6xwYJk5a2m8"}
+    ],
+    "Miércoles":[
+      {name:"Circuito cardio (burpees/caja)", time:"18 min", video:"https://www.youtube.com/watch?v=TU8QYVW0gDU"}
+    ],
+    "Jueves":[
+      {name:"Dominadas asistidas + bíceps", time:"4x", video:"https://www.youtube.com/watch?v=eGo4IYlbE5g"}
+    ],
+    "Viernes":[
+      {name:"Elevación cadera + femoral", time:"4x12", video:"https://www.youtube.com/watch?v=E1GJbZ2iQbQ"}
+    ]
+  },
+  "4": {
+    "Lunes":[{name:"Press banca con pausa + tríceps", time:"5x5", video:"https://www.youtube.com/watch?v=rT7DgCr-3pg"}],
+    "Martes":[{name:"Bici + zancadas", time:"15 + 4x12", video:"https://www.youtube.com/watch?v=QOVaHwm-Q6U"}],
+    "Miércoles":[{name:"HIIT carrera + caja", time:"20 min", video:"https://www.youtube.com/watch?v=ml6cT4AZdqI"}],
+    "Jueves":[{name:"Jalón neutro + curl martillo", time:"4x12", video:"https://www.youtube.com/watch?v=zC3nLlEvin4"}],
+    "Viernes":[{name:"Buenos días + femoral", time:"4x10", video:"https://www.youtube.com/watch?v=vj2w851ZHRM"}]
+  },
+  "5": {
+    "Lunes":[{name:"Superserie pecho/tríceps", time:"30 min", video:"https://www.youtube.com/watch?v=pQGgYxZr6iI"}],
+    "Martes":[{name:"Prensa + extensora + bici suave", time:"30 min", video:"https://www.youtube.com/watch?v=IZxyjW7MPJQ"}],
+    "Miércoles":[{name:"Cardio continuo", time:"25 min", video:"https://www.youtube.com/watch?v=6Uyf6YcZ0R8"}],
+    "Jueves":[{name:"Remo polea + curl barra Z", time:"4x10", video:"https://www.youtube.com/watch?v=GxjTgGdQ2BA"}],
+    "Viernes":[{name:"Peso muerto rumano + glute bridge", time:"4x10", video:"https://www.youtube.com/watch?v=Dl2o5CX7G1Y"}]
+  }
+};
